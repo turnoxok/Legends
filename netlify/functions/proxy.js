80a096a93932073fb46f2cdc0a50a67eeb685e4c
turnoxok <turnoxok@gmail.com>
2025-09-18 09:27:17 -0300

    Create proxy.js

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function(event) {
+  try {
+    // URL del Apps Script que desplegaste
+    const googleScriptUrl = "https://script.google.com/macros/s/AKfycbyzo0C9FmurlTJfPLXg_S9b07xFyMTJ4JqhJ6nxpcGz-o6HlGiZJZRmSn-vhn-6MqZU/exec";
+
+    const response = await fetch(googleScriptUrl, {
+      method: "POST",
+      body: event.body,
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: data
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
